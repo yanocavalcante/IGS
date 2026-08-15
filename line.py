32981b264725036871fb3f41a7a55eea1f743343
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2026-08-15 18:53:05 -0300

    feat: creating basic instantiable objects

---
+++ line.py
@@ -0,0 +1,6 @@
+from graphic_obj import GraphicObject, ObjectType
+
+
+class Line(GraphicObject):
+    def __init__(self, name: str, id: int, type: ObjectType, coords: list[int]):
+        super().__init__(name, id, type, coords)
