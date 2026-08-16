0942e884a10b0db1c8de714dd59c43acfa286c78
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2026-08-15 21:09:35 -0300

    feat: creating window class

---
+++ window.py
@@ -0,0 +1,6 @@
+class Window():
+    def __init__(self, xwmin, ywmin, xwmax, ywmax) -> None:
+        self.__xwmin = xwmin
+        self.__ywmin = ywmin
+        self.__xwmax = xwmax
+        self.__ywmax = ywmax
