9d47c80c10cc249e7755dd25724f8421f36c5f4c
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2026-08-17 23:19:19 -0300

    feat: adding superficial movement buttons

--- src/interface.py
+++ src/interface.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
     QMainWindow,
+    QMessageBox,
     QHBoxLayout,
     QVBoxLayout,
     QPushButton,
@@ -42,16 +43,50 @@ class SGIInterface(QMainWindow):
         self.main_layout.addWidget(self.menu)
 
         zoomIn_btn = QPushButton('Zoom +')
-        zoomIn_btn.clicked.connect(self.open_input_dialog)
+        zoomIn_btn.clicked.connect(self.zoom_in)
         self.menu_layout.addWidget(zoomIn_btn)
 
         zoomOut_btn = QPushButton('Zoom -')
-        zoomOut_btn.clicked.connect(self.open_input_dialog)
+        zoomOut_btn.clicked.connect(self.zoom_out)
         self.menu_layout.addWidget(zoomOut_btn)
+
+        moveUp_btn = QPushButton('Up')
+        moveUp_btn.clicked.connect(self.move_up)
+        self.menu_layout.addWidget(moveUp_btn)
+
+        moveDown_btn = QPushButton('Down')
+        moveDown_btn.clicked.connect(self.move_down)
+        self.menu_layout.addWidget(moveDown_btn)
+
+        moveLeft_btn = QPushButton('Left')
+        moveLeft_btn.clicked.connect(self.move_left)
+        self.menu_layout.addWidget(moveLeft_btn)
+
+        moveRight_btn = QPushButton('Right')
+        moveRight_btn.clicked.connect(self.move_right)
+        self.menu_layout.addWidget(moveRight_btn)
 
         create_btn = QPushButton('Create New Object')
         create_btn.clicked.connect(self.create_object)
         self.menu_layout.addWidget(create_btn)
+
+    def zoom_in(self):
+        QMessageBox.information(self, "IGS", "You zoomed in!")
+
+    def zoom_out(self):
+        QMessageBox.information(self, "IGS", "You zoomed out!")
+
+    def move_up(self):
+        QMessageBox.information(self, "IGS", "You moved up!")
+
+    def move_down(self):
+        QMessageBox.information(self, "IGS", "You moved down!")
+
+    def move_left(self):
+        QMessageBox.information(self, "IGS", "You moved left!")
+
+    def move_right(self):
+        QMessageBox.information(self, "IGS", "You moved right!")
 
     def create_object(self):
         dialog = QDialog(self)
