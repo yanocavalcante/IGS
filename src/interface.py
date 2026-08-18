9ccdca36e9266fdc58844ff95f3e89a99272045b
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2026-08-18 16:16:05 -0300

    enhance: layout distribution of menu buttons

--- src/interface.py
+++ src/interface.py
@@ -6,6 +6,8 @@ from PyQt6.QtWidgets import (
     QHBoxLayout,
     QVBoxLayout,
     QPushButton,
+    QLabel,
+    QGridLayout,
     QDialog,
     QFormLayout,
     QLineEdit,
@@ -19,6 +21,7 @@ class SGIInterface(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWindowTitle('Interactive Graphic System')
+        self.setGeometry(100, 100, 1024, 768)
 
         self.display_file = []
 
@@ -34,41 +37,83 @@ class SGIInterface(QMainWindow):
         self.viewport = QWidget()
         self.viewport_layout = QVBoxLayout()
         self.viewport.setLayout(self.viewport_layout)
-        self.main_layout.addWidget(self.viewport)
+        self.main_layout.addWidget(self.viewport, 4)
 
     def create_menu(self):
         self.menu = QWidget()
-        self.menu_layout = QVBoxLayout()
-        self.menu.setLayout(self.menu_layout)
-        self.main_layout.addWidget(self.menu)
+        self.menu_layout = QVBoxLayout(self.menu)
 
-        zoomIn_btn = QPushButton('Zoom +')
-        zoomIn_btn.clicked.connect(self.zoom_in)
-        self.menu_layout.addWidget(zoomIn_btn)
+        self.main_layout.addWidget(self.menu, 1)
 
-        zoomOut_btn = QPushButton('Zoom -')
-        zoomOut_btn.clicked.connect(self.zoom_out)
-        self.menu_layout.addWidget(zoomOut_btn)
+        self.menu_layout.addWidget(QLabel("Zoom"))
+        self.zoom_layout = QGridLayout()
 
-        moveUp_btn = QPushButton('Up')
-        moveUp_btn.clicked.connect(self.move_up)
-        self.menu_layout.addWidget(moveUp_btn)
+        self.add_menu_button(
+            self.zoom_layout,
+            "Zoom +",
+            self.zoom_in,
+            0, 0
+        )
 
-        moveDown_btn = QPushButton('Down')
-        moveDown_btn.clicked.connect(self.move_down)
-        self.menu_layout.addWidget(moveDown_btn)
+        self.add_menu_button(
+            self.zoom_layout,
+            "Zoom -",
+            self.zoom_out,
+            0, 1
+        )
 
-        moveLeft_btn = QPushButton('Left')
-        moveLeft_btn.clicked.connect(self.move_left)
-        self.menu_layout.addWidget(moveLeft_btn)
+        self.menu_layout.addLayout(self.zoom_layout)
 
-        moveRight_btn = QPushButton('Right')
-        moveRight_btn.clicked.connect(self.move_right)
-        self.menu_layout.addWidget(moveRight_btn)
+        self.menu_layout.addWidget(QLabel("Navigation"))
+        self.navigation_layout = QGridLayout()
 
-        create_btn = QPushButton('Create New Object')
-        create_btn.clicked.connect(self.create_object)
-        self.menu_layout.addWidget(create_btn)
+        self.add_menu_button(
+            self.navigation_layout,
+            "Up",
+            self.move_up,
+            1, 1
+        )
+
+        self.add_menu_button(
+            self.navigation_layout,
+            "Left",
+            self.move_left,
+            2, 0
+        )
+
+        self.add_menu_button(
+            self.navigation_layout,
+            "Down",
+            self.move_down,
+            2, 1
+        )
+
+        self.add_menu_button(
+            self.navigation_layout,
+            "Right",
+            self.move_right,
+            2, 2
+        )
+
+        self.menu_layout.addLayout(self.navigation_layout)
+
+        self.menu_layout.addWidget(QLabel("Objects"))
+        self.objects_layout = QGridLayout()
+
+        self.add_menu_button(
+            self.objects_layout,
+            "Create New Object",
+            self.create_object,
+            0, 0
+        )
+
+        self.menu_layout.addLayout(self.objects_layout)
+        self.menu_layout.addStretch()
+
+    def add_menu_button(self, layout, text, callback, row, column):
+        button = QPushButton(text)
+        button.clicked.connect(callback)
+        layout.addWidget(button, row, column)
 
     def zoom_in(self):
         QMessageBox.information(self, "IGS", "You zoomed in!")
@@ -89,6 +134,7 @@ class SGIInterface(QMainWindow):
         QMessageBox.information(self, "IGS", "You moved right!")
 
     def create_object(self):
+        object_dict = {"Name": "", "Type": "", "Coords": ""}
         dialog = QDialog(self)
         dialog.setWindowTitle("Create New Object")
 
@@ -96,33 +142,44 @@ class SGIInterface(QMainWindow):
         dialog_form_layout = QFormLayout()
 
         name = QLineEdit()
-        object_type = QComboBox()
+        name.setPlaceholderText("e.g. MyFirstObject")
 
-        object_type.addItems([
+        coords = QLineEdit()
+        coords.setPlaceholderText("e.g. (1,1),(2,2),(4,4)")
+
+        dialog_form_layout.addRow("Name:", name)
+
+        obj_type = QComboBox()
+        obj_type.setPlaceholderText("Object Type")
+        obj_type.addItems([
             "Point",
             "Line",
             "Polygon"
         ])
+        dialog_form_layout.addWidget(obj_type)
 
-        coords_widget = QWidget()
-        coords_layout = QFormLayout(coords_widget)
+        dialog_form_layout.addRow("Coords:", coords)
 
         button_box = QDialogButtonBox(
-            QDialogButtonBox.StandardButton.Ok |
-            QDialogButtonBox.StandardButton.Cancel
+            QDialogButtonBox.StandardButton.Cancel |
+            QDialogButtonBox.StandardButton.Ok
         )
 
-        dialog_form_layout.addRow("Name:", name)
-        dialog_form_layout.addRow("Type:", object_type)
-
         dialog_layout.addLayout(dialog_form_layout)
-        dialog_layout.addWidget(coords_widget)
+
         dialog_layout.addWidget(button_box)
 
-        button_box.accepted.connect(dialog.accept)
         button_box.rejected.connect(dialog.reject)
+        button_box.accepted.connect(dialog.accept)
 
-        dialog.exec()
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            object_dict["Name"] = name.text()
+            object_dict["Type"] = obj_type.currentText()
+            object_dict["Coords"] = coords.text()
+
+            print(object_dict) # For testing - Remove
+
+            return object_dict
 
     def open_input_dialog(self):
         name, ok = QInputDialog.getText(
