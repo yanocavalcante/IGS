fb8dfc3ae801c1773f1ba8b54cf0f84dc5c27976
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2026-08-18 23:59:22 -0300

    feat: draw method for 'Point' class

--- src/point.py
+++ src/point.py
@@ -4,3 +4,9 @@ from graphic_obj import GraphicObject, ObjectType
 class Point(GraphicObject):
     def __init__(self, name: str, id: int, type: ObjectType, coords: list[int]):
         super().__init__(name, id, type, coords)
+
+    def draw(self, painter):
+        painter.drawPoint(self.coords[0], self.coords[1])
+
+    def transform(self):
+        pass
