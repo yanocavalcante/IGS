db9b1bfebc444cac429076abb070be4a3fa64a04
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2026-08-19 00:26:10 -0300

    feat: support for drawing lines

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,6 @@
 from interface import SGIInterface
 from point import Point
+from line import Line
 from obj_type import ObjectType
 
 class Controller:
@@ -11,7 +12,7 @@ class Controller:
             case "Point":
                 self.sgi.add_object_to_df(Point(obj_dict["Name"], int(1), ObjectType.POINT, obj_dict["Coords"]))
             case "Line":
-                pass
+                self.sgi.add_object_to_df(Line(obj_dict["Name"], int(2), ObjectType.LINE, obj_dict["Coords"]))
             case "Polygon":
                 pass
 
